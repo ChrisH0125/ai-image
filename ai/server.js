05978b7a741fa205be5d5b90c920aa795ed3fd8b
ChrisH0125 <ho.chris.cs@gmail.com>
2025-05-21 13:22:48 -0400

    Use variables to initialize openAI stk

--- ai/server.js
+++ ai/server.js
@@ -1,3 +1,8 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
+import { Configuration, OpenAIApi} from "openai";
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI,
+});
