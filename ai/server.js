fa8220f30c50882a9721690ddaafdf4b5e6116ed
ChrisH0125 <ho.chris.cs@gmail.com>
2025-05-21 13:44:51 -0400

    Fixes on server

--- ai/server.js
+++ ai/server.js
@@ -1,13 +1,10 @@
+import OpenAI from 'openai';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-import { Configuration, OpenAIApi } from 'openai';
-
-const configuration = new Configuration({
+const openai = new OpenAI({
   apiKey: process.env.OPENAI,
 });
-
-const openai = new OpenAIApi(configuration);
 
 import express from 'express';
 import cors from 'cors';
@@ -20,14 +17,19 @@ app.use(express.json());
 app.post('/dream', async (req, res) => {
     const prompt = req.body.prompt;
 
-    const aiResponse = await openai.createImage({
-      prompt,
-      n: 1,
-      size: '1024x1024',
-    });
+    try {
+      const aiResponse = await openai.images.generate({
+        prompt,
+        n: 1,
+        size: '1024x1024',
+      });
 
-    const image = aiResponse.data.data[0].url;
-    res.send({ image });
+      const image = aiResponse.data[0].url;
+      res.send({ image });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Image generation failed.');
+    }
 });
 
-app.listen(8080, () => console.log('test'));
+app.listen(8080, () => console.log('Server listening on http://localhost:8080'));
