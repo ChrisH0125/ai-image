394a6628899750967abfd18dbfcb0ce56b718607
ChrisH0125 <ho.chris.cs@gmail.com>
2025-05-21 13:23:40 -0400

    import expressjs

--- ai/server.js
+++ ai/server.js
@@ -1,8 +1,12 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-import { Configuration, OpenAIApi} from "openai";
+import OpenAI, { Configuration, OpenAIApi} from "openai";
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI,
 });
+
+const openai = new OpenAIApi(configuration);
+
+import express from "express";
